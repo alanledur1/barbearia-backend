48ef94cc432ff49416ebd291df0ef07785522954
alanledur1 <ledur77conta2@gmail.com>
2025-10-06 22:57:40 -0300

    criacao de rota unificada para admin e client

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,13 @@ import serviceRoutes from './service.routes';
 import appointmentRoutes from './appointment.routes';
 import authRoutes from './auth.routes'; 
 import adminRoutes from './admin.routes';
+import { UnifiedLoginController } from '../controllers/unifiedLogin.controller';
 /* import adminRoutes from './admin.routes'; */
 
 const router = Router();
+const unifiedLoginController = new UnifiedLoginController();
 
+router.post('/login', (req, res) => unifiedLoginController.login(req, res));
 router.use('/clients', clientRoutes);
 router.use('/services', serviceRoutes);
 router.use('/appointments', appointmentRoutes);
