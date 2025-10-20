a24a69a50b4280f001a002cf66bc536d09d45c06
alanledur1 <ledur77conta2@gmail.com>
2025-10-20 18:31:43 -0300

    alteracoes 5

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
 import app from "./app";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-// LOG PARA VERIFICAR A URL
 console.log("--- Verificando DATABASE_URL ---");
 console.log("URL do Banco de Dados:", process.env.DATABASE_URL);
 console.log("--- Fim da Verificação ---");
 
-// Converte a porta para número (necessário no TypeScript)
 const PORT = Number(process.env.PORT) || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server is running on port ${PORT}`);
-});
-
-// Captura erros globais
-process.on("uncaughtException", (err) => {
-  console.error("❌ Uncaught Exception:", err);
-});
-
-process.on("unhandledRejection", (reason) => {
-  console.error("⚠️ Unhandled Rejection:", reason);
 });
