20cbd5f9d4f2545bafaa142b97ec6edf673a8f32
Alan Ledur <ledur77conta2@gmail.com>
2026-02-24 16:06:26 -0300

    fix: cast req.params.id to string6

--- src/prisma/db.ts
+++ src/prisma/db.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pkg from "pg";
 
-export const prisma = new PrismaClient();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+// Agora estamos passando o adapter válido, então o PrismaClient não está "vazio"
+export const prisma = new PrismaClient({
+  adapter,
+} as any);
